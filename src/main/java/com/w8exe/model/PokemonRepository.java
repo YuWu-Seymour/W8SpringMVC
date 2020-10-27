a6e3878500e2804c5afc139608e7af6c6182b8cc
YuWu-Seymour <ywuu0108@student.monash.edu>
2020-10-27 15:18:25 +1100

    Initial commit

---
+++ src/main/java/com/w8exe/model/PokemonRepository.java
@@ -0,0 +1,17 @@
+package com.w8exe.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
+    @Query(value = "SELECT p FROM Pokemon p WHERE " +
+            "lower(p.pokename) LIKE lower(CONCAT('%', :keyword, '%')) OR " +
+            "lower(p.poketype) LIKE lower(CONCAT('%', :keyword, '%')) OR " +
+            "lower(p.pokespeed) LIKE lower(CONCAT('%', :keyword, '%'))")
+    public List<Pokemon> search(@Param("keyword") String keyword);
+}
